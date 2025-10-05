b8af20d1cd65e70b7c432bc9fcc39a95071f4fac
Sofía Fernández <soffiafdz@Lazarus.>
2025-10-05 13:28:02 -0400

    Submodule changes and backup-full

--- dev/core/paths.py
+++ dev/core/paths.py
@@ -5,45 +5,6 @@ paths.py
 Set of pathlib Paths to be loaded by the modules.
 Generate daily Markdown files for Vimwiki reference and PDF generation.
 
-├── journal
-│   ├── archive
-│   ├── bin
-│   ├── inbox
-│   ├── latex
-│   ├── md
-│   │   ├── 2015
-│   │   ├── 2016
-│   │   ├── 2017
-│   │   ├── 2018
-│   │   ├── 2019
-│   │   ├── 2021
-│   │   ├── 2022
-│   │   ├── 2023
-│   │   ├── 2024
-│   │   └── 2025
-│   ├── pdf
-│   ├── txt
-│   │   ├── 2015
-│   │   ├── 2016
-│   │   ├── 2017
-│   │   ├── 2018
-│   │   ├── 2019
-│   │   ├── 2021
-│   │   ├── 2022
-│   │   ├── 2023
-│   │   ├── 2024
-│   │   └── 2025
-│   └── metadata.json
-├── code
-│   ├── md2json
-│   ├── md2pdf
-│   ├── md2wiki
-│   └── txt2md
-├── vignettes
-└── wiki
-    ├── log
-    ├── people
-    └── snippets
 Notes
 ==============
 - Manages two types of 750w metadata entry formats.
@@ -53,42 +14,38 @@ from pathlib import Path
 
 # ----- Project directory -----
 ROOT: Path = Path(__file__).resolve().parents[1]
+DATA_DIR = ROOT / "data"  # Personal data (private)
 
 # ---- Dev ----
 DEV_DIR = ROOT / "dev"
 
 # --- Converters/Pipeline ---
 FORMATTING_SCRIPT = DEV_DIR / "bin" / "init_format"
-# TODO: Adapt these. New logic for structure
-# TXT2MD_DIR: Path = ROOT / "code" / "txt2md"
-# MD2WIKI_DIR: Path = ROOT / "code" / "md2wiki"
+TEX_DIR = ROOT / "templates" / "tex"
 
 # --- Database ---
-DB_PATH = ROOT / "palimpsest.db"
 ALEMBIC_DIR = DEV_DIR / "migrations"
+DB_DIR = DATA_DIR / "metadata"
+DB_PATH = DB_DIR / "palimpsest.db"
 
 # ---- Journal ----
-# LATEX_DIR = ROOT / "journal" / "latex"
-JOURNAL_DIR = ROOT / "journal"
+JOURNAL_DIR = DATA_DIR / "journal"
 INBOX_DIR = JOURNAL_DIR / "inbox"
 ARCHIVE_DIR = JOURNAL_DIR / "sources" / "archive"
 TXT_DIR = JOURNAL_DIR / "sources" / "txt"
 MD_DIR = JOURNAL_DIR / "content" / "md"
 PDF_DIR = JOURNAL_DIR / "content" / "pdf"
 
-# ---- Backup & Logs & Temp ----
-BACKUP_DIR = ROOT / "backups"
+# ---- Logs & Temp & Backups----
 LOG_DIR = ROOT / "logs"
 TMP_DIR = ROOT / "tmp"
-
-# ---- Templates ----
-TEX_DIR = ROOT / "templates" / "tex"
+BACKUP_DIR = ROOT / "backups"
 
 # ---- Vignettes ----
-VIGNETTES_DIR = ROOT / "vignettes"
+VIGNETTES_DIR = DATA_DIR / "vignettes"
 
 # ---- Vimwiki ----
-WIKI_DIR = ROOT / "wiki"
+WIKI_DIR = DATA_DIR / "wiki"
 INVENTORY_DIR = WIKI_DIR / "inventory"
 PEOPLE_DIR = WIKI_DIR / "people"
 SNIPPETS_DIR = WIKI_DIR / "snippets"
